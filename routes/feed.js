d12159ff9d2306d1d3865a7f44f5670c110b4d55
Jad <gr4798@wayne.edu>
2020-10-27 13:40:22 -0400

    Added search bar feature

--- routes/feed.js
+++ routes/feed.js
@@ -2,36 +2,70 @@ const express = require("express");
 const router = express.Router();
 const path = require("path");
 const PostBook = require("../models/post.model");
-const authenticate= require('../middleware/authenticate')
+const authenticate = require('../middleware/authenticate');
+const { toASCII } = require("punycode");
 
 router.get("/feed", authenticate, function (req, res, next) {
-    PostBook.find({}, function(err, data){
+    PostBook.find({}, function (err, data) {
         res.render('feed', {
             books: data
         });
-    }).sort({"_id":-1})
-  
+    }).sort({ "_id": -1 })
+
 });
 
-router.post("/filter", authenticate, function (req, res, next){
+router.post("/filter", authenticate, function (req, res, next) {
     var fltrsubject = req.body.fltrsubject;
     var fltrcondition = req.body.fltrcondition;
 
-    if(fltrsubject != '' && fltrcondition != ''){
-        var fltrParameter = { $and: [{Subject:fltrsubject},{Condition:fltrcondition}]}
-    }else if(fltrsubject == '' && fltrcondition != ''){
-        var fltrParameter = {Condition:fltrcondition}
-    }else if(fltrsubject != '' && fltrcondition == '') {
-        var fltrParameter = {Subject:fltrsubject}
-    }else{
+    if (fltrsubject != '' && fltrcondition != '') {
+        var fltrParameter = { $and: [{ Subject: fltrsubject }, { Condition: fltrcondition }] }
+    } else if (fltrsubject == '' && fltrcondition != '') {
+        var fltrParameter = { Condition: fltrcondition }
+    } else if (fltrsubject != '' && fltrcondition == '') {
+        var fltrParameter = { Subject: fltrsubject }
+    } else {
         var fltrParameter = {}
     }
 
-    PostBook.find(fltrParameter, function(err, data){
+    PostBook.find(fltrParameter, function (err, data) {
         res.render('feed', {
             books: data
         });
-    }).sort({"_id":-1})
+    }).sort({ "_id": -1 })
 })
+
+
+// GET BOOK BY TITLE
+
+router.post("/search", authenticate, function (req, res, next) {
+    var title = req.body.title;
+
+    var fltrParameter = { Title: title }
+
+    PostBook.find(fltrParameter, function (err, data) {
+        res.render('feed', {
+            books: data
+        });
+    }).sort({ "_id": -1 })
+})
+
+/*router.post("/search", async (req, res) => {
+    const Title = req.body.title;
+
+    try {
+        const book = await PostBook.find({ Title });
+        if (!book) {
+            return res.status(404).send();
+        }
+        res.render('feed',{
+            books: Title
+        });
+
+    } catch (e) {
+        res.status(500).send()
+    }
+    
+})*/
 
 module.exports = router;
